a8c64b2041c39ebe12383d1272f1ec58139d2121
Rasilgon <rasilgon@gmail.com>
2018-08-29 17:13:35 +0200

    Renamed file and function: replaced locations by sites. Call argument for warnings.

--- ProfoundData/R/getSites.R
+++ ProfoundData/R/getSites.R
@@ -1,12 +1,12 @@
-# @title A get locations function
-# @description  A function to provide information on available locations in the
+# @title A get sites function
+# @description  A function to provide information on available sites in the
 # ProfoundData database
-# @param location a character string providing the name of the location (optional)
-# @return a table with the locations if not arguments are passed. Otherwise, it returns the
-# location ID.
+# @param site a character string providing the name of the site (optional)
+# @return a table with the sites if not arguments are passed. Otherwise, it returns the
+# site ID.
 # @export
 # @examples \dontrun{
-# locations <- getLocations()
+# sites <- getsites()
 # }
 # @note To report errors in the package or the data, please use the issue tracker
 # in the github repository of TG2 https://github.com/COST-FP1304-PROFOUND/TG2/issues
@@ -14,21 +14,21 @@
 # or use this google form http://goo.gl/forms/e2ZQCiZz4x
 # @keywords ProfoundData
 # @author Ramiro Silveyra Gonzalez
-getLocations <- function(location = NULL){
+getsites <- function(site = NULL){
   conn <- try(makeConnection(), T)
   if ('try-error' %in% class(conn)){
-    stop("Invalid database connection")
+    stop("Invalid database connection", call. = FALSE)
   }
   table <- RSQLite::dbGetQuery(conn, "SELECT * FROM SITES")
   RSQLite::dbDisconnect(conn)
-  if(!is.null(location)){
-    if (location %in% table[["site"]]){
-      sites <- table[table$site==location,]$site_id
-#    }else if(location %in% table[["site2"]]){
-#      sites <- table[table$site2==location,]$site_id
+  if(!is.null(site)){
+    if (site %in% table[["site"]]){
+      sites <- table[table$site==site,]$site_id
+#    }else if(site %in% table[["site2"]]){
+#      sites <- table[table$site2==site,]$site_id
 #      sites <-  sites[!is.na(sites)]
-    }else if (location %in% table[["site_id"]]){
-      sites <- location
+    }else if (site %in% table[["site_id"]]){
+      sites <- site
     }else{
       sites <- 0
     }
