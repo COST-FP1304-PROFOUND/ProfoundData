02e2a31629fd30ef8ca8f6cdadc14161b29c2264
Rasilgon <rasilgon@gmail.com>
2018-08-29 17:26:33 +0200

    Replaced locations by sites. Call argument for warnings/stop.

--- ProfoundData/R/parseQuery.R
+++ ProfoundData/R/parseQuery.R
@@ -1,7 +1,7 @@
 # @title A function to parse query data from a DB
 #
 # @description TODO
-# @param location a character string providing the name of the location
+# @param site a character string providing the name of the site
 # @param dataset a character string providing the name one of the available datasets
 # @return tmp a object containing only needed parameters for executing the query
 # @note To report errors in the package or the data, please use the issue tracker
@@ -10,14 +10,14 @@
 # or use this google form http://goo.gl/forms/e2ZQCiZz4x
 # @author Ramiro Silveyra Gonzalez
 
-parseQuery <- function(dataset, location,  forcingDataset = NULL,
+parseQuery <- function(dataset, site, forcingDataset = NULL,
                        forcingCondition = NULL, species = NULL, variables = NULL, period = NULL,
                        aggregated = NULL, FUN = mean, automaticPanels = F, quality = NULL ,
                        decreasing = TRUE, collapse = TRUE){
 
   conn <- try(makeConnection(), T)
   if ('try-error' %in% class(conn)){
-    stop("Invalid database connection")
+    stop("Invalid database connection", call. = FALSE)
   }else{
     RSQLite::dbDisconnect(conn)
   }
@@ -54,7 +54,7 @@ parseQuery <- function(dataset, location,  forcingDataset = NULL,
   }
   message("Checking dataset")
   # create the item.dataset
-  tmp <- list( dataset = dataset, location = location,
+  tmp <- list( dataset = dataset, site = site,
                variables = variables,
                period = period,
                aggregated = aggregated, FUN = FUN, automaticPanels = automaticPanels,
@@ -65,41 +65,41 @@ parseQuery <- function(dataset, location,  forcingDataset = NULL,
 
   if(is.null(tmp[["variables"]])) tmp[["dropVariables"]] <- F
   dataset <- getDatasets(tmp[["dataset"]])
-  if (!dataset) stop("Invalid dataset. Please use browseData() to see the available datasets")
+  if (!dataset) stop("Invalid dataset. Please use browseData() to see the available datasets", call. = FALSE)
   if (tmp[["dataset"]] == "VERSION" || grepl("METADATA", tmp[["dataset"]]) || tmp[["dataset"]] == "POLICY" || tmp[["dataset"]] == "SOURCE" ){
-    stop(paste("Use browseData for accesing", tmp[["dataset"]]))
+    stop(paste("Use browseData for accesing", tmp[["dataset"]]), call. = FALSE)
   }
-  message("Checking location")
-  if(is.null(tmp[["location"]])){
+  message("Checking site")
+  if(is.null(tmp[["site"]])){
     if (tmp[["dataset"]] == "SITES" || tmp[["dataset"]] == "SITEDESCRIPTION" ){
       tmp[["item"]] <- paste( "SELECT * FROM", tmp[["dataset"]] ,sep = " ")
     }else{
-      stop("Bug!")
+      stop("Please provide a site name", call. = FALSE)
     }
-  }else if(!is.null(tmp[["location"]])){
-    tmp[["location"]] <- getLocations(tmp[["location"]])
-    if (!tmp[["location"]])stop("Invalid location. Please use browseData to see the available locations")
+  }else if(!is.null(tmp[["site"]])){
+    tmp[["site"]] <- getsites(tmp[["site"]])
+    if (!tmp[["site"]])stop("Invalid site. Please use browseData to see the available sites", call. = FALSE)
     message("Checking variables")
   #  cat(tmp[["dataset"]]); cat(tmp[["variables"]])
     tmp[["variablesChecked"]] <- parseVariables(tmp[["dataset"]], tmp[["variables"]])
     # Here code for CO2 and SITES. Based on version do something or the other.
     # Check data availability
     message("Checking the availability")
-    available <- checkAvailable(dataset = tmp[["dataset"]], location=tmp[["location"]])
+    available <- checkAvailable(dataset = tmp[["dataset"]], site=tmp[["site"]])
     if (!available){
       if (tmp$dataset == "CO2_ISIMIP"){
-        # Chekc if locations are there
-        if(!is.null(location)){
-          location <- getLocations(location)
-          if (!location){stop("Invalid location. Please use browseData to see the available locations")}
+        # Chekc if sites are there
+        if(!is.null(site)){
+          site <- getsites(site)
+          if (!site){stop("Invalid site. Please use browseData to see the available sites", call. = FALSE)}
         }
         tmp[["item"]] <- paste( "SELECT * FROM", tmp[["dataset"]],sep = " ")
-        tmp[["location"]]<- 99
+        tmp[["site"]]<- 99
       }else if (!tmp[["dataset"]] == "SITES" ){
-        stop("The dataset is not available for the location. Please use browseData() to check data availability")
+        stop("The dataset is not available for the site. Please use browseData() to check data availability", call. = FALSE)
       }
     }
-    tmp[["item"]] <- paste( "SELECT * FROM", paste(tmp[["dataset"]], tmp[["location"]], sep = "_") ,sep = " ")
+    tmp[["item"]] <- paste( "SELECT * FROM", paste(tmp[["dataset"]], tmp[["site"]], sep = "_") ,sep = " ")
   }
   # return the tmp object
   return(tmp)
