f85cf6b40f688ac883502947b7d535e69f3ff0ca
Rasilgon <rasilgon@gmail.com>
2018-08-29 16:56:19 +0200

    Replaced locations by sites. Typos

--- ProfoundData/R/checkAvailable.R
+++ ProfoundData/R/checkAvailable.R
@@ -1,13 +1,13 @@
 # @title A check availability function used by browseData and parseQuery
-# @description  A fucntion to check availability of a dataset for a location.
+# @description  A fucntion to check availability of a dataset for a site.
 # It is used by browseData and parseQuery
 # @param dataset a character string providing the name of the dataset
-# @param location a character string providing the name of the location
+# @param site a character string providing the name of the site
 # @return a numeric boolean
-checkAvailable <- function(dataset, location){
+checkAvailable <- function(dataset, site){
   tmp <-0
-  dataset_location <- paste(dataset, location, sep="_")
-  if(getDatasets(dataset_location)){
+  dataset_site <- paste(dataset, site, sep="_")
+  if(getDatasets(dataset_site)){
     tmp <- 1
   }
   return(tmp)
