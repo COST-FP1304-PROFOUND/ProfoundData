342446741fcab18fa233cdd4a7d02f32a40ce4b8
Rasilgon <rasilgon@gmail.com>
2018-08-29 16:54:17 +0200

    Improved help. Replaced locations by sites

--- ProfoundData/R/ProfoundData.R
+++ ProfoundData/R/ProfoundData.R
@@ -2,39 +2,47 @@
 #' @aliases ProfoundData
 #' @name ProfoundData-package
 #' @title Overview of the functions in the ProfoundData package
-#' @description The ProfoundData package provides functions to access the Profound
-#' database. The database is designed to provide data needed by forest
-#' models. The package allow users to explore the available dataset and locations
-#' in the database, as well as download and visualize data. ProfoundData should be particularly
-#' useful for users working with models on a R environment.
+#' @description The ProfoundData package provides functions to explore, visualize and get data from the PROFOUND
+#' database. The package is particularly convenient for working with forest models on an R environment.
 #'
-#' Below is a list of the functions grouped by theme. See the vignette for more
-#' information and some examples (you can open it by running this command:
-#' \code{vignette('ProfoundDataVignette')})
+#' A brief description of the PROFOUND database is avalaible in the vignette 'PROFOUNDdatabase' (
+#' you can open it by running this command: \code{vignette('PROFOUNDdatabase')}).
+#' A report of the data contained in the database is
+#' provided in the vignette 'PROFOUNDsites' (\code{vignette('PROFOUNDsites')}).
 #'
-#' @section I. Browse the database: Functions to explore what datasets, variables, locations are available.
+#' Below is a list of the package's functions grouped by theme. See the package
+#' vignette for more information and examples (\code{vignette('ProfoundData')}).
+#'
+#'
+#' @section I. Browse the database: Functions to explore the database.
 #' \itemize{
 #'   \item \code{\link{browseData}} To check
 #'   \itemize{
-#'    \item  available locations
+#'    \item  available sites
 #'    \item  available datasets
 #'    \item  available variables for a dataset
-#'    \item  available datasets for a location
+#'    \item  available datasets for a site
 #'    \item  database version
 #'    \item  metadata
 #'    \item  policy
 #'    \item  source
 #'    \item  site description
 #'    }
+#'    \item \code{\link{summarizeData}} To obtain
+#'     \itemize{
+#'    \item  data overviews
+#'    \item  data summaries
+#'    }
+#'
 #' }
 #'
-#' @section II. Download data: Functions to download data. Data can be download for one location and one dataset at a time.
+#' @section II. Download data: Functions to download data. Data can be downloaded for one site and one dataset at a time.
 #' \itemize{
 #'  \item \code{\link{getData}} To download data
 #' }
 #'
 #'
-#' @section III. Visualize data: Functions to plot data from the database
+#' @section III. Visualize data: Functions to plot data from the database.
 #' \itemize{
 #'   \item \code{\link{plotData}} To plot data
 #' }
@@ -43,13 +51,12 @@
 #' \itemize{
 #'  \item \code{\link{setDB}} To set the connection to the database
 #'  \item \code{\link{getDB}} To retrieve the filepath to the database
-#'   \item \code{\link{writeSim2netCDF}} To write netCDF-files
-#'   \item \code{\link{summarizeData}} To summarize data from the database.
 #'   \item \code{\link{queryDB}} To pass self-defined queries
+#'   \item \code{\link{writeSim2netCDF}} To write netCDF-files
 #' }
 #'
 #' @author Except where indicated otherwise, the functions in this package were
-#'  written by Ramiro Silveyra Gonzalez
+#'  written by Ramiro Silveyra Gonzalez.
 #' @section Acknowledgements:
 #' TODO
 #' @keywords package
