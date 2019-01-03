b3720ffa63287cf4a406d8fb465048320006e1ce
Rasilgon <rasilgon@gmail.com>
2019-01-03 11:42:02 +0100

    Replaced forcingConditions by forcingCondition

--- PROFOUND database/createDB/Create_CO2_ISIMIP_Views.R
+++ PROFOUND database/createDB/Create_CO2_ISIMIP_Views.R
@@ -10,7 +10,7 @@ dbGetQuery(db, "CREATE VIEW CO2_ISIMIP AS
            SITESID_master.site,
            CO2_ISIMIP_master.site_id,
            CO2_ISIMIP_master.year,
-           CO2_ISIMIP_master.forcingConditions,
+           CO2_ISIMIP_master.forcingCondition,
            CO2_ISIMIP_master.co2_ppm
            FROM CO2_ISIMIP_master INNER JOIN SITESID_master ON CO2_ISIMIP_master.site_id = SITESID_master.site_id"
 )
@@ -28,7 +28,7 @@ for (j in 1:length(ids[,1])){
                        "SITESID_master.site, ",
                        ids[j,], " AS site_id, ",
                        "CO2_ISIMIP_master.year, ",
-                       "CO2_ISIMIP_master.forcingConditions, ",
+                       "CO2_ISIMIP_master.forcingCondition, ",
                        "CO2_ISIMIP_master.co2_ppm ",
                        "FROM CO2_ISIMIP_master INNER JOIN SITESID_master ON CO2_ISIMIP_master.site_id = SITESID_master.site_id ",
                        sep = "")
@@ -40,32 +40,32 @@ dbDisconnect(db)
 
 
 db <- dbConnect(SQLite(), dbname=myDB)
-forcingConditions <- dbGetQuery(db, "SELECT forcingConditions FROM (SELECT DISTINCT forcingConditions FROM CO2_ISIMIP_master)")
+forcingCondition <- dbGetQuery(db, "SELECT forcingCondition FROM (SELECT DISTINCT forcingCondition FROM CO2_ISIMIP_master)")
 ids <- dbGetQuery(db, "SELECT site_id FROM (SELECT DISTINCT site_id FROM SITES_master)")
 
-for (i in 1:length(forcingConditions[,1])){
-  if ( paste("CO2_ISIMIP_", forcingConditions[i,],sep = "") %in% dbListTables(db))  dbSendQuery(db, paste("DROP VIEW CO2_ISIMIP_", forcingConditions[i,],sep = ""))
-  dbGetQuery(db, paste("CREATE VIEW CO2_ISIMIP_", forcingConditions[i,], " AS ",
+for (i in 1:length(forcingCondition[,1])){
+  if ( paste("CO2_ISIMIP_", forcingCondition[i,],sep = "") %in% dbListTables(db))  dbSendQuery(db, paste("DROP VIEW CO2_ISIMIP_", forcingCondition[i,],sep = ""))
+  dbGetQuery(db, paste("CREATE VIEW CO2_ISIMIP_", forcingCondition[i,], " AS ",
                        "SELECT CO2_ISIMIP_master.record_id, ",
                        "SITESID_master.site, ",
                        "CO2_ISIMIP_master.site_id, ",
                        "CO2_ISIMIP_master.year, ",
-                       "CO2_ISIMIP_master.forcingConditions, ",
+                       "CO2_ISIMIP_master.forcingCondition, ",
                        "CO2_ISIMIP_master.co2_ppm ",
-                       "FROM CO2_ISIMIP_master INNER JOIN SITESID_master ON CO2_ISIMIP_master.site_id = SITESID_master.site_id WHERE CO2_ISIMIP_master.forcingConditions = '",
-                       forcingConditions[i,], "'", sep = "")
+                       "FROM CO2_ISIMIP_master INNER JOIN SITESID_master ON CO2_ISIMIP_master.site_id = SITESID_master.site_id WHERE CO2_ISIMIP_master.forcingCondition = '",
+                       forcingCondition[i,], "'", sep = "")
   )
   for (j in 1:length(ids[,1])){
-    if ( paste("CO2_ISIMIP_",forcingConditions[i,],"_", ids[j,],sep = "") %in% dbListTables(db))  dbSendQuery(db, paste("DROP VIEW CO2_ISIMIP_",forcingConditions[i,],"_", ids[j,],sep = ""))
-    dbGetQuery(db, paste("CREATE VIEW CO2_ISIMIP_", forcingConditions[i,],"_", ids[j,], " AS ",
+    if ( paste("CO2_ISIMIP_",forcingCondition[i,],"_", ids[j,],sep = "") %in% dbListTables(db))  dbSendQuery(db, paste("DROP VIEW CO2_ISIMIP_",forcingCondition[i,],"_", ids[j,],sep = ""))
+    dbGetQuery(db, paste("CREATE VIEW CO2_ISIMIP_", forcingCondition[i,],"_", ids[j,], " AS ",
                          "SELECT CO2_ISIMIP_master.record_id, ",
                          "SITESID_master.site, ",
                          ids[j,], " AS site_id, ",
                          "CO2_ISIMIP_master.year, ",
-                         "CO2_ISIMIP_master.forcingConditions, ",
+                         "CO2_ISIMIP_master.forcingCondition, ",
                          "CO2_ISIMIP_master.co2_ppm ",
-                         "FROM CO2_ISIMIP_master INNER JOIN SITESID_master ON CO2_ISIMIP_master.site_id = SITESID_master.site_id WHERE CO2_ISIMIP_master.forcingConditions = '",
-                         forcingConditions[i,],"'", sep = "")
+                         "FROM CO2_ISIMIP_master INNER JOIN SITESID_master ON CO2_ISIMIP_master.site_id = SITESID_master.site_id WHERE CO2_ISIMIP_master.forcingCondition = '",
+                         forcingCondition[i,],"'", sep = "")
     )
   }
 }
